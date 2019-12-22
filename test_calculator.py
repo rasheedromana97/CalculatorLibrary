1141e0974bb3f47e49350eb16d78f69a64aa8476
Romana Rasheed <rasheed.romana@yahoo.com>
2019-12-22 14:56:23 -0600

    Add Unit Tests

---
+++ test_calculator.py
@@ -0,0 +1,15 @@
+#The prefix "test" in the name of the file is important because that is how pytest will know to conduct unit tests.
+"""
+Unit tests for the calculator library
+"""
+
+import calculator
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calculator.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calculator.subtract(4, 2)
